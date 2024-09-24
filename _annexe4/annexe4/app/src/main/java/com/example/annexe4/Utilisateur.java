583f2dd9fec0f381ca0a9f8efb418033659febfd
Alek Del Balso <alekdelbalso@gmail.com>
2024-09-24 13:45:44 -0400

    annexe4

---
+++ _annexe4/annexe4/app/src/main/java/com/example/annexe4/Utilisateur.java
@@ -0,0 +1,11 @@
+package com.example.annexe4;
+
+public class Utilisateur {
+
+    public String prenom;
+    public String nom;
+    public Utilisateur(String prenom, String nom){
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
