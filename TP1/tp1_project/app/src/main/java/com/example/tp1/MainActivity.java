325b9543f587e5ef378f3220bcc833af0dfc398b
Alek-DB <Alekdelbalso@gmail.com>
2024-10-16 16:49:31 -0400

    tp1

--- TP1/tp1_project/app/src/main/java/com/example/tp1/MainActivity.java
+++ TP1/tp1_project/app/src/main/java/com/example/tp1/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.tp1;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.media3.common.MediaItem;
+import androidx.media3.common.Player;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.ui.PlayerView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -17,7 +20,27 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Playlist playlist;
+
+    private ExoPlayer exoP;
+    private PlayerView vue;
+
+    private Button back;
+    private Button back_seconds;
+    private Button pause;
+    private Button forward_seconds;
+    private Button forward;
+
+    private int song_index = 0;
+    private long song_position = 0;
 
 
     @Override
@@ -32,25 +55,95 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        back = findViewById(R.id.back);
+        back_seconds = findViewById(R.id.back_seconds);
+        pause = findViewById(R.id.pause);
+        forward_seconds = findViewById(R.id.forward_seconds);
+        forward = findViewById(R.id.forward);
+
+
+        back.setOnClickListener(v -> {
+            if(song_index != 0) play_song(--song_index, 0);
+        });
+        back_seconds.setOnClickListener(v -> {
+            exoP.seekTo(exoP.getCurrentPosition() - 10000);
+        });
+        pause.setOnClickListener(v -> {
+            if(exoP.isPlaying())exoP.pause();
+            else exoP.play();
+        });
+        forward_seconds.setOnClickListener(v -> {
+            exoP.seekTo(exoP.getCurrentPosition() + 10000);
+        });
+        forward.setOnClickListener(v -> {
+            if(song_index != playlist.music.size()-1) play_song(++song_index, 0);
+            System.out.println("hell");
+            System.out.println(playlist.music.size());
+        });
+
+
+
+        exoP = new ExoPlayer.Builder(this).build();
+        exoP.addListener(new Player.Listener() {
+            @Override
+            public void onPlaybackStateChanged(int state) {
+                if (state == Player.STATE_ENDED) {
+                    if (exoP.hasNextMediaItem()) {
+                        exoP.seekToNextMediaItem();
+                        exoP.play();
+                    } else {
+
+                        System.out.println("Fin de la playlist.");
+                    }
+                }
+            }
+        });
+
+
+        vue = findViewById(R.id.playerView);
+        vue.setPlayer(exoP);
+        vue.setUseController(false);
+
+        get_songs();
+    }
+
+
+    private void play_song(int which, long where){
+        exoP.seekTo(which, where);
+        exoP.play();
+    }
+
+    private void get_songs(){
         RequestQueue rq = Volley.newRequestQueue(this);
         String url = "https://api.jsonbin.io/v3/b/661ab8b1acd3cb34a837f284?meta=false";
-        //ATELIER 2B
-
         StringRequest sr = new StringRequest(url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Gson gson = new Gson();
-                Playlist musiques = gson.fromJson(response, Playlist.class) ;
+                playlist = gson.fromJson(response, Playlist.class) ;
 
-                System.out.println(musiques.music.size());
+                try {
+                    FileInputStream fis = openFileInput("fichier.ser");
+                    ObjectInputStream ois = new ObjectInputStream(fis);
 
-                for (int i = 0; i < musiques.music.size(); i++){
-                    System.out.println(musiques.music.get(i).getAlbum());
+                    song_index = ois.read();
+                    song_position = ois.readLong();
+
+                    ois.close();
+                    fis.close();
+                } catch (IOException ignored) {
+                    System.out.println("crashed in get_songs");
                 }
 
+                System.out.println("got songs");
 
+                for(Track m: playlist.music){
+                    exoP.addMediaItem(MediaItem.fromUri(m.getSource()));
+                }
 
+                exoP.prepare();
 
+                play_song(song_index, song_position);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -58,24 +151,34 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("Crashed");
             }
         });
-
         rq.add(sr);
-
-
-
-
-
     }
 
 
     @Override
     protected void onStop() {
         super.onStop();
+
+        exoP.pause();
+        try {
+            FileOutputStream fos = openFileOutput("fichier.ser", MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.write(song_index);
+            System.out.println("pos" + exoP.getCurrentPosition());
+            oos.writeLong(exoP.getCurrentPosition());
+
+            oos.close();
+            fos.close();
+        } catch (IOException ignored) {
+            System.out.println("crashed in onStop");
+        }
     }
 
-
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onRestart() {
+        super.onRestart();
+
+        play_song(song_index, song_position);
     }
 }
